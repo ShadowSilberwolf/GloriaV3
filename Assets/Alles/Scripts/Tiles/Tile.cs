e3f39470b309fa27d6f1c9e2e5048683763deb26
ShadowSilberwolf <hoffmanntobias2000@gmail.com>
2021-08-09 18:20:26 +0200

    GrassTile brah

--- Assets/Alles/Scripts/Tiles/Tile.cs
+++ Assets/Alles/Scripts/Tiles/Tile.cs
@@ -6,9 +6,18 @@ public class Tile : MonoBehaviour
 {
     [SerializeField] private Color baseColor, offsetColor;
     [SerializeField] private SpriteRenderer renderer;
-
+    [SerializeField] private GameObject Hightlight;
     public void Init(bool isOffset)
     {
         renderer.color = isOffset ? offsetColor : baseColor;
+    }
+
+    private void OnMouseEnter()
+    {
+        Hightlight.SetActive(true);
+    }
+    private void OnMouseExit()
+    {
+        Hightlight.SetActive(false);
     }
 }

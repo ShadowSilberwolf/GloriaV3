659cc2e469b18a7e1624a300016d0dcdd225777b
ShadowSilberwolf <hoffmanntobias2000@gmail.com>
2021-08-16 18:52:04 +0200

    Faction

--- Assets/Manner/UnitManager.cs
+++ Assets/Manner/UnitManager.cs
@@ -1,18 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class UnitManager : MonoBehaviour
 {
     public static UnitManager Instance;
-
+    private List<ScriptableUnit> Unitys;
     void Awake()
     {
-        Instance = this;        
+        Instance = this;
+
+        Unitys = Resources.LoadAll<ScriptableUnit>("Units").ToList();
     }
 
+    public void SpawnHeroes()
+    {
+        var heroCount = 1;
 
+        for(int i = 0; i < heroCount; i++)
+        {
 
+        }
+    }
 
+    private T GetRandomUnit<T>(Faction faction) where T : BaseUnits
+    {
+        return (T)Unitys.Where(u => u.faction == faction).OrderBy(o => Random.value).First().UnitPrefab;
+    }
 
 }

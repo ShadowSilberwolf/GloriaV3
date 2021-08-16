23d647970dbfa6e57f87774dda9ee8d27e33879a
ShadowSilberwolf <hoffmanntobias2000@gmail.com>
2021-08-16 18:57:38 +0200

    gthwru

--- Assets/Alles/Units/Player1/BasePlayer1.cs
+++ Assets/Alles/Units/Player1/BasePlayer1.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BasePlayer1 : MonoBehaviour
+public class BasePlayer1 : BaseUnits
 {
     // Start is called before the first frame update
     void Start()

db78f448507240d475949e93ec729ba31417a333
benjamin255 <77289293+benjamin255@users.noreply.github.com>
2021-09-13 14:57:01 +0200

    Du Huan

---
+++ Assets/Alles/Scripts/Männer/Grid1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+
+public class Grid<T>
+{
+    private int width;
+    private int height;
+    private float v;
+    private Vector3 zero;
+    private Func<Grid<PathNode>, int, int, PathNode> p;
+
+    public Grid(int width, int height, float v, Vector3 zero, Func<Grid<PathNode>, int, int, PathNode> p)
+    {
+        this.width = width;
+        this.height = height;
+        this.v = v;
+        this.zero = zero;
+        this.p = p;
+    }
+}

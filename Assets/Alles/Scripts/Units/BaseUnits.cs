64c989ff58311ab5ccaa514c0926202742cc8a2a
ShadowSilberwolf <hoffmanntobias2000@gmail.com>
2021-10-04 16:28:40 +0200

    oh mein gott wir kommen weiter

--- Assets/Alles/Scripts/Units/BaseUnits.cs
+++ Assets/Alles/Scripts/Units/BaseUnits.cs
@@ -7,4 +7,19 @@ public class BaseUnits : MonoBehaviour
     public Tile OccupiedTile;
     public Faction faction;
     public string unitName;
+
+    public int health;
+    public int Damage;
+    public int Armor;
+
+    public void Attack(BaseUnits target)
+    {
+        target.health = target.health - (Damage - target.Armor);
+        if(target.health <= 0)
+        {
+            Destroy(target);
+        }
+    }
+
+    
 }
